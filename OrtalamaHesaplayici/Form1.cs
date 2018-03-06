2bef98cde0246a465d06441485070ae2966ee7fe
erdemsilver <gumuserdem@outlook.com>
2018-03-06 23:53:37 +0300

    Csharp Ortalama Hesaplayici

---
+++ OrtalamaHesaplayici/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrtalamaHesaplayici
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sayi1, sayi2, sonuc;
+
+            sayi1 = Convert.ToDouble(textBox1.Text);
+            sayi2 = Convert.ToDouble(textBox2.Text);
+            sonuc = (sayi1 + sayi2) / 2;
+            listBox1.Items.Add(sonuc);
+
+            if (sonuc > 50)
+            {
+                MessageBox.Show("Gectiniz !");
+            }
+            else 
+            {
+                MessageBox.Show("Kaldiniz !");
+            }
+            
+
+
+
+        }
+    }
+}
